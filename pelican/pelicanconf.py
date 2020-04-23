33a6e287ef1566c5298ff6764b5eb6a9e49f317e
SUGIMOTO Norimitsu <dictoss@live.jp>
2020-04-24 00:05:15 +0900

    fix sub-menu path.

--- pelican/pelicanconf.py
+++ pelican/pelicanconf.py
@@ -54,13 +54,13 @@ ARTICLE_URL = '{slug}.html'
 
 # provided as examples, they make ‘clean’ urls. used by MENU_INTERNAL_PAGES.
 TAGS_URL           = 'tags'
-TAGS_SAVE_AS       = 'tags/index.html'
+TAGS_SAVE_AS       = 'tags.html'
 AUTHORS_URL        = 'authors'
-AUTHORS_SAVE_AS    = 'authors/index.html'
+AUTHORS_SAVE_AS    = 'authors.html'
 CATEGORIES_URL     = 'categories'
-CATEGORIES_SAVE_AS = 'categories/index.html'
+CATEGORIES_SAVE_AS = 'categories.html'
 ARCHIVES_URL       = 'archives'
-ARCHIVES_SAVE_AS   = 'archives/index.html'
+ARCHIVES_SAVE_AS   = 'archives.html'
 
 # use those if you want pelican standard pages to appear in your menu
 MENU_INTERNAL_PAGES = (
