7a6528fcd26c5b20142ed2cf6e315f6bd520e317
SUGIMOTO Norimitsu <dictoss@live.jp>
2020-04-23 23:30:13 +0900

    fix link to relative path.

--- pelican/pelicanconf.py
+++ pelican/pelicanconf.py
@@ -19,6 +19,7 @@ DATE_FORMATS = {
     'en': '%Y-%m-%d',
 }
 
+PREFIX_DIR = 'xddc/'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -50,15 +51,18 @@ DISPLAY_FOOTER = True
 DISPLAY_HOME   = True
 DISPLAY_MENU   = True
 
+#
+ARTICLE_URL = 'xddc/{slug}.html'
+
 # provided as examples, they make ‘clean’ urls. used by MENU_INTERNAL_PAGES.
-TAGS_URL           = 'tags'
-TAGS_SAVE_AS       = 'tags/index.html'
-AUTHORS_URL        = 'authors'
-AUTHORS_SAVE_AS    = 'authors/index.html'
-CATEGORIES_URL     = 'categories'
-CATEGORIES_SAVE_AS = 'categories/index.html'
-ARCHIVES_URL       = 'archives'
-ARCHIVES_SAVE_AS   = 'archives/index.html'
+TAGS_URL           = PREFIX_DIR + 'tags'
+TAGS_SAVE_AS       = PREFIX_DIR + 'tags/index.html'
+AUTHORS_URL        = PREFIX_DIR + 'authors'
+AUTHORS_SAVE_AS    = PREFIX_DIR + 'authors/index.html'
+CATEGORIES_URL     = PREFIX_DIR + 'categories'
+CATEGORIES_SAVE_AS = PREFIX_DIR + 'categories/index.html'
+ARCHIVES_URL       = PREFIX_DIR + 'archives'
+ARCHIVES_SAVE_AS   = PREFIX_DIR + 'archives/index.html'
 
 # use those if you want pelican standard pages to appear in your menu
 MENU_INTERNAL_PAGES = (
--- pelican/publishconf.py
+++ pelican/publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'cddc'
+SITEURL = 'https://dictoss.github.io/xddc'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
